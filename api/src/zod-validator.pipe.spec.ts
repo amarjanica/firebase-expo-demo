9b9d2d6e1b9309d1d4fec073c89e482a8600d125
Ana Bujan <ana@eisberg-labs.com>
2025-09-05 14:03:20 +0200

    cleanup

--- api/src/zod-validator.pipe.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ZodValidatorPipe } from './zod-validator.pipe';
-
-describe('ZodValidatorPipe', () => {
-  it('should be defined', () => {
-    expect(new ZodValidatorPipe()).toBeDefined();
-  });
-});
